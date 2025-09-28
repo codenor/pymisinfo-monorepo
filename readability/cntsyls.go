869971c5ba8435652aa064fe111314089505118c
Jed Saxon <jed-sax@proton.me>
2025-09-28 14:52:47 +1000

    remove go code :(

--- readability/cntsyls.go
+++
@@ -1,7 +0,0 @@
-package read
-
-// CntSyls estimates the syllable counts in a text from the number of characters in words.
-func CntSyls(text string) int {
-	letterCnt, digitCnt, _ := CntChars(text)
-	return letterCnt/3 + digitCnt
-}
